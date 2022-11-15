1c3146bb28394d29db82d6254a008349099a60de
ElvisNIG <elvis.chukwuani@aun.edu.ng>
2022-11-15 23:40:19 +0100

    Message

---
+++ home.js
@@ -0,0 +1,12 @@
+/**
+ *  document.getElementById("nextf").addEventListener("click", myFunction);
+      function myFunction() {
+        window.location.href="health.html";
+      }
+      document.getElementById("nexta").addEventListener("click", myFunction);
+      function myFunction() {
+        window.location.href="finance.html";
+      }
+ * 
+ * 
+ */
---
+++ test.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    $("#uploadBtn1").on("change", function () {
+   
+    $("#application").attr('checked','checked');
+   
+   });
+   $("#uploadBtn2").on("change", function () {
+   
+    $("#application2").attr('checked','checked');
+   
+   });
+   });
